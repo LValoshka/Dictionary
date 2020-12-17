b1b2a12c7088c6f77969387f72e9ee025429d4dd
Denis Sergeev <denis@stanum.us>
2020-12-17 13:52:05 +0300

    Renamed tab

--- src/main/java/by/bsu/dictionary/ui/MainLayout.java
+++ src/main/java/by/bsu/dictionary/ui/MainLayout.java
@@ -6,7 +6,6 @@ import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dialog.Dialog;
-import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -18,10 +17,8 @@ public class MainLayout extends AppLayout {
     }
 
     private void createHeader() {
-        H1 logo = new H1("Logo=)");
-        logo.addClassName("logo");
-
         Button textButton = new Button(VaadinIcon.TEXT_LABEL.create());
+        textButton.addThemeVariants(ButtonVariant.LUMO_ERROR);
         textButton.addClickListener(e -> UI.getCurrent().navigate("text"));
         Button tableButton = new Button(VaadinIcon.TABLE.create());
         tableButton.addClickListener(e -> UI.getCurrent().navigate("words"));
@@ -32,13 +29,12 @@ public class MainLayout extends AppLayout {
             dialog.setDraggable(true);
             dialog.open();
         });
-        Button statButton = new Button(VaadinIcon.STAR_HALF_LEFT.create());
+        Button statButton = new Button("Tag");
         statButton.addClickListener(e -> UI.getCurrent().navigate("stat"));
         Button nameTagButton = new Button("Name-tag");
         nameTagButton.addClickListener(e -> UI.getCurrent().navigate("nameTag"));
         Button tagsButton = new Button("Tags");
         tagsButton.addClickListener(e -> UI.getCurrent().navigate("tags"));
-
 
         HorizontalLayout header = new HorizontalLayout(textButton, tableButton, info,
                 statButton, nameTagButton, tagsButton);
